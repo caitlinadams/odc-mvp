8a665609f64886561dceb6223c52cf6c07bd3fcc
Caitlin Adams <25995927+caitlinadams@users.noreply.github.com>
2025-10-21 16:27:06 +1030

    Update styles for radar backscatter to include conversion to dB, and a single false-colour styling

--- config/s1_styles.py
+++ config/s1_styles.py
@@ -1,5 +1,5 @@
-HH = {
-    "name": "HH",
+hh_linear = {
+    "name": "hh_linear",
     "title": "Backscatter HH",
     "abstract": "Backscatter HH",
     "components": {
@@ -10,8 +10,41 @@ HH = {
     "scale_range": [0.02, 0.4],
 }
 
-HV = {
-    "name": "HV",
+hh_db = {
+    "name": "hh_db",
+    "title": "Backscatter HH (dB)",
+    "abstract": "Backscatter HH (dB)",
+    "additional_bands": ["HH_gamma0"],
+    "components": {
+        "red": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "HH_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "blue": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "HH_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "green": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "HH_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+    },
+}
+
+hv_linear = {
+    "name": "hv_linear",
     "title": "Backscatter HV",
     "abstract": "Backscatter HV",
     "components": {
@@ -22,8 +55,41 @@ HV = {
     "scale_range": [0.02, 0.4],
 }
 
-VV = {
-    "name": "VV",
+hv_db = {
+    "name": "hv_db",
+    "title": "Backscatter HV (dB)",
+    "abstract": "Backscatter HV (dB)",
+    "additional_bands": ["HV_gamma0"],
+    "components": {
+        "red": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "HV_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "blue": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "HV_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "green": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "HV_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+    },
+}
+
+vv_linear = {
+    "name": "vv_linear",
     "title": "Backscatter VV",
     "abstract": "Backscatter VV",
     "components": {
@@ -34,8 +100,41 @@ VV = {
     "scale_range": [0.02, 0.4],
 }
 
-VH = {
-    "name": "VH",
+vv_db = {
+    "name": "vv_db",
+    "title": "Backscatter VV (dB)",
+    "abstract": "Backscatter VV (dB)",
+    "additional_bands": ["VV_gamma0"],
+    "components": {
+        "red": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VV_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "blue": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VV_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "green": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VV_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+    },
+}
+
+vh_linear = {
+    "name": "vh_linear",
     "title": "Backscatter VH",
     "abstract": "Backscatter VH",
     "components": {
@@ -44,6 +143,65 @@ VH = {
         "blue": {"VH_gamma0": 1},
     },
     "scale_range": [0.02, 0.4],
+}
+
+vh_db = {
+    "name": "vh_db",
+    "title": "Backscatter VH (dB)",
+    "abstract": "Backscatter VH (dB)",
+    "additional_bands": ["VH_gamma0"],
+    "components": {
+        "red": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VH_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "blue": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VH_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+        "green": {
+            "function": "s1_style_functions.db",
+            "kwargs": {
+                "band": "VH_gamma0",
+                "scale_from": (-17, 6),
+                "scale_to": (0, 255),
+            },
+        },
+    },
+}
+
+vv_vh_false_colour_linear = {
+    "name": "vv_vh_false_colour_linear",
+    "title": "VV+VH False Colour",
+    "abstract": "VV+VH False Colour",
+    "additional_bands": ["VV_gamma0", "VH_gamma0"],
+    "components": {
+        "red": {
+            "VV_gamma0": 1.0,
+            "scale_range": [0.0, 0.28],
+        },
+        "green": {
+            "VH_gamma0": 1.0,
+            "scale_range": [0.0, 0.06],
+        },
+        "blue": {
+            "function": "datacube_ows.band_utils.band_quotient",
+            "mapped_bands": True,
+            "kwargs": {
+                "band1": "VH_gamma0",
+                "band2": "VV_gamma0",
+                "scale_from": [0.0, 0.49],
+            },
+        },
+    },
 }
 
 mask = {
